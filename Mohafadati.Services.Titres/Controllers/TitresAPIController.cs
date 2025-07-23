0b28e266c2fbfeaf0f52c8e52861ade18c47077e
JT1-1-1 <l.yassir@proton.me>
2025-07-23 15:11:19 +0100

    Titre Fonciers Service + logs
    Services de verification de disponibilité du titre

---
+++ Mohafadati.Services.Titres/Controllers/TitresAPIController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Mohafadati.Services.Titres.Data;
+using Mohafadati.Services.Titres.Models;
+using Mohafadati.Services.Titres.Models.Dto;
+
+namespace Mohafadati.Services.Titres.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TitresAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public TitresAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Titre> objList = _db.Titres.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] TitreDto obj)
+        {
+
+            try 
+            {
+                List<Titre> TitreContent = _db.Titres.ToList();
+                Titre? titre = TitreContent.Find((element)=> element.ConservationFonciere.Equals(obj.ConservationFonciere)
+                                                             && element.Indice.Equals(obj.Indice) 
+                                                             && element.IndiceSpecial.Equals(obj.IndiceSpecial)
+                                                             && element.NumeroTitre == obj.NumeroTitre);
+                
+                if (titre is null)
+                {
+                    throw new Exception("Ce titre n'existe pas!");
+                }
+                return Ok(obj);
+
+            }
+            catch (Exception ex) 
+            {
+                return NotFound( new {message = ex.Message} );
+            }
+
+
+
+            
+            //    List<Titre> objList = _db.Titres.ToList();
+            //Titre? T = objList.Find((element) => element.ConservationFonciere.Equals(obj.ConservationFonciere) 
+            //&& element.Indice.Equals(obj.Indice) 
+            //&& element.IndiceSpecial.Equals(obj.IndiceSpecial)
+            //&& element.NumeroTitre == obj.NumeroTitre);
+
+            
+            //if (T != null)
+            //{
+            //    TitreDto Titre = new()
+            //    {
+            //        ConservationFonciere = T.ConservationFonciere,
+            //        Indice = T.Indice,
+            //        IndiceSpecial = T.IndiceSpecial,
+            //        NumeroTitre = T.NumeroTitre
+            //    };
+            //    return Titre;
+            //}
+            //else return null;
+            
+
+        }
+    }
+}
---
+++ Mohafadati.Services.Titres/Middlewares/LogsMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using Mohafadati.Services.Titres.Models.Dto;
+
+namespace Mohafadati.Services.Titres.Middlewares
+{
+    public class LogsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string FilePath;
+        public LogsMiddleware(RequestDelegate next)
+        { 
+            _next = next;
+            FilePath = "C:\\Users\\AdminLachmachi\\Desktop\\Logs.txt";
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+           
+           
+            context.Request.EnableBuffering();
+            string bodyAsString = "";
+            using (var reader = new StreamReader(
+                context.Request.Body,
+                encoding: System.Text.Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                bufferSize: 1024,
+                leaveOpen: true))
+            {
+                bodyAsString = await reader.ReadToEndAsync();
+                context.Request.Body.Position = 0; 
+            }
+
+
+            string RequestLog = $"--> Requete faite le {DateTime.Now} -- Par: {context.Connection.RemoteIpAddress?.ToString()} -- Path: {context.Request.Path} -- Methode: {context.Request.Method} -- Contenu: {bodyAsString}{Environment.NewLine}";
+            await File.AppendAllTextAsync(FilePath, RequestLog);
+
+
+
+
+
+            var originalResponseBody = context.Response.Body;
+            using (var memoryStream = new MemoryStream())
+            {
+                context.Response.Body = memoryStream;
+                await _next(context);
+
+                memoryStream.Position = 0;
+                string responseBody = await new StreamReader(memoryStream).ReadToEndAsync();
+
+                memoryStream.Position = 0;
+                await memoryStream.CopyToAsync(originalResponseBody);
+
+                string ResponseLog = $"<-- Reponse faite le {DateTime.Now} -- Code: {context.Response.StatusCode} -- Contenu: {responseBody}{Environment.NewLine}";
+                await File.AppendAllTextAsync(FilePath, ResponseLog);
+                
+            }
+
+
+
+
+
+            
+        }
+    }
+}
